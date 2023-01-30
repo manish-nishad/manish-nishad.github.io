fb7c45b26a3b04e64faace77486a428b10ea0e7f
Manish <maknish1993@gmail.com>
2023-01-31 01:32:04 +0530

    Initial commit

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,101 @@
+import {
+  Box,
+  Flex,
+  Avatar,
+  HStack,
+  IconButton,
+  Button,
+  Link,
+  Menu,
+  MenuButton,
+  useDisclosure,
+  useColorModeValue,
+  Stack,
+} from '@chakra-ui/react';
+import { HamburgerIcon, CloseIcon } from '@chakra-ui/icons';
+import ColorModeToggle from './ColorModeToggle';
+import img from "../Data/mn-logo.jpg";
+
+
+// const Links = ['Home', 'About', 'Skills', 'Projects', 'Contact'];
+
+const NavLink = ({children}) => (
+  <Link
+    fontSize={'18px'}
+    fontWeight={700}
+    px={3}
+    py={1}
+    rounded={'md'}
+    _hover={{
+      textDecoration: 'none',
+      bg: useColorModeValue('blue.200', 'blue.700'),
+    }}
+    >
+    {children}
+  </Link>
+);
+
+export default function Navbar({handleHome, handleAbout, handleSkills, handleProjects, handleContacts}) {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  return (
+    <>
+      <Box bg={useColorModeValue('blue.50', 'blue.900')} px={4} position={'fixed'} zIndex={3}  width={'100%'} >
+        <Flex h={20} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Open Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Avatar
+                size={'lg'}
+                src={img}
+                link={'https://maknish.github.io'}
+                cursor="pointer"
+            />
+          </HStack>
+          <Flex alignItems={'center'}>
+            <Menu>
+              <MenuButton
+                as={Button}
+                rounded={'full'}
+                variant={'link'}
+                cursor={'pointer'}
+                minW={0}>
+              </MenuButton>
+
+            <HStack
+            as={'nav'}
+            spacing={4}
+            display={{ base: 'none', md: 'flex' }}>
+             <Box onClick={handleHome}> <NavLink>Home</NavLink></Box>
+             <Box onClick={handleAbout}>  <NavLink>About</NavLink></Box>
+             <Box onClick={handleSkills}> <NavLink>Skills</NavLink></Box>
+             <Box onClick={handleProjects}> <NavLink>Projects</NavLink></Box>
+             <Box onClick={handleContacts}>    <NavLink>Contacts</NavLink></Box>
+             <Box><NavLink><Link href='https://drive.google.com/file/d/13qjwZkSn8jyAPmQKP2FUjQHi83__Tzgr/view?usp=share_link'>Resume</Link></NavLink></Box>
+             
+            </HStack>
+            </Menu>
+          <ColorModeToggle />
+          </Flex>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              <NavLink><Box onClick={handleAbout}>About</Box></NavLink>
+              <NavLink><Box onClick={handleHome}>Home</Box></NavLink>
+              <NavLink><Box onClick={handleSkills}>Skills</Box></NavLink>
+              <NavLink><Box onClick={handleProjects}>Projects</Box></NavLink>
+              <NavLink><Box onClick={handleContacts}>Contacts</Box></NavLink>
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
+  );
+}
---
+++ src/Pages/Contact.jsx
@@ -0,0 +1,203 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+import {
+    Container, Flex, Box,
+    Heading,
+    Text,
+    IconButton,
+    Button,
+    VStack,
+    HStack,
+    Wrap,
+    WrapItem,
+    Link,
+    useColorModeValue,
+  } from '@chakra-ui/react';
+  import { MdPhone, MdEmail, MdLocationOn } from 'react-icons/md';
+  import { BsGithub, BsLinkedin, BsTwitter } from 'react-icons/bs';
+
+const Contact = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_w93i0jk', 'template_2sgd1xn', form.current, 'lHC2pYqAm1mO-hkP2')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <>
+    <Container maxW="full" mt={0} centerContent overflow="hidden">
+        <Flex>
+          <Box
+            bg={useColorModeValue('blue.50', 'blue.400')}
+            color="Black"
+            borderRadius="lg"
+            m={{ sm: 4, md: 16, lg: 10 }}
+            p={{ sm: 5, md: 5, lg: 16 }}>
+            <Box p={4}>
+              <Wrap spacing={{ base: 20, sm: 3, md: 5, lg: 20 }}>
+                <WrapItem>
+                  <Box>
+                    <Heading>Contact</Heading>
+                    <Text mt={{ sm: 3, md: 3, lg: 5 }} color="black.500">
+                      Fill up the form below to contact
+                    </Text>
+                    <Box py={{ base: 5, sm: 5, md: 8, lg: 10 }}>
+                      <VStack pl={0} spacing={3} alignItems="flex-start">
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="220px"
+                          position={''}
+                          variant="ghost"
+                          color="black"
+                          _hover={{ border: '2px solid #1C6FEB' }}
+                          leftIcon={<MdPhone color="black" size="20px" />}>
+                          +91 8718812542
+                        </Button>
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="220px"
+                          variant="ghost"
+                          color="black"
+                          _hover={{ border: '2px solid #1C6FEB' }}
+                          leftIcon={<MdEmail color="black" size="20px" />}>
+                          maknish1993@gmail.com
+                        </Button>
+                        <Button
+                          size="md"
+                          height="48px"
+                          width="220px"
+                          variant="ghost"
+                          color="#black"
+                          _hover={{ border: '2px solid #1C6FEB' }}
+                          leftIcon={<MdLocationOn color="black" size="20px" />}>
+                          Chhattisgarh, India
+                        </Button>
+                      </VStack>
+                    </Box>
+                    <HStack
+                      mt={{ lg: 10, md: 10 }}
+                      spacing={5}
+                      px={5}
+                      alignItems="flex-start">
+                        <Link href="https://twitter.com/manishnishad18" target={'blank'}>
+                        <IconButton
+                        aria-label="facebook"
+                        variant="ghost"
+                        size="lg"
+                        isRound={true}
+                        _hover={{ bg: '#0D74FF' }}
+                        icon={<BsTwitter size="28px" />}
+                      />
+                        </Link>
+                    
+                      <Link href="https://github.com/MAKNISH" target={'blank'}>
+                      <IconButton
+                        aria-label="github"
+                        variant="ghost"
+                        size="lg"
+                        isRound={true}
+                        _hover={{ bg: '#0D74FF' }}
+                        icon={<BsGithub size="28px" />}
+                      />
+                      </Link>
+                      
+                      <Link href="https://www.linkedin.com/in/manish-nishad/" target={'blank'}>
+                      <IconButton
+                        aria-label="discord"
+                        variant="ghost"
+                        size="lg"
+                        isRound={true}
+                        _hover={{ bg: '#0D74FF' }}
+                        icon={<BsLinkedin size="28px" />}
+                      />
+                      </Link>
+                      
+                    </HStack>
+                  </Box>
+                </WrapItem>
+                <WrapItem>
+                  <Box bg="white" borderRadius="lg">
+                    <Box m={8} color="#0B0E3F">
+                      <HStack spacing={5}>
+                        {/* <FormControl id="name">
+                          <FormLabel>Your Name</FormLabel>
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement
+                              pointerEvents="none"
+                              children={<BsPerson color="gray.800" />}
+                            />
+                            <Input type="text" size="md" name="user_name"/>
+                          </InputGroup>
+                        </FormControl>
+                        <FormControl id="name">
+                          <FormLabel>Mail</FormLabel>
+                          <InputGroup borderColor="#E0E1E7">
+                            <InputLeftElement
+                              pointerEvents="none"
+                              children={<MdOutlineEmail color="gray.800" />}
+                            />
+                            <Input type="text" size="md" name="user_email"/>
+                          </InputGroup>
+                        </FormControl>
+                        <FormControl id="name">
+                          <FormLabel>Message</FormLabel>
+                          <Textarea
+                            borderColor="gray.300"
+                            _hover={{
+                              borderRadius: 'gray.300',
+                            }}
+                            placeholder="message"
+                          />
+                        </FormControl>
+                        <FormControl id="name" float="right">
+                          <Button
+                            value="Send"
+                            type="submit"
+                            variant="solid"
+                            bg="#0D74FF"
+                            color="white"
+                            _hover={{}}>
+                            Send Message
+                          </Button>
+                        </FormControl> */}
+                        <form ref={form} onSubmit={sendEmail}>
+                          <label style={{}}>Name</label><br></br>
+                          <input style={{border: "1px solid #E0E1E7", borderRadius: "5px", type: "text", size:"md"}} type="text" name="user_name" /><br></br>
+                          <label>Email</label><br></br>
+                          <input style={{border: "1px solid #E0E1E7", borderRadius: "5px", type: "text", size:"md"}} type="email" name="user_email" /><br></br>
+                          <label>Message</label><br></br>
+                          <textarea name="message" style={{border:"1px solid #E0E1E7", borderRadius: "5px"}}></textarea><br></br>
+                          <input style={{variant:"solid", backgroundColor:"#4299e1", padding:"5px 20px", borderRadius:"5px"}} type="submit" value="Send" />
+                        </form>
+{/* 
+                        rounded={'full'}
+              size={'lg'}
+              fontWeight={'normal'}
+              px={6}
+              colorScheme={'blue'}
+              bg={'blue.400'}
+              position={''}
+              _hover={{ bg: 'blue.500' }} */}
+                      </HStack>
+                    </Box>
+                  </Box>
+                </WrapItem>
+              </Wrap>
+            </Box>
+          </Box>
+        </Flex>
+      </Container>
+    </>
+  );
+};
+
+export default Contact;
